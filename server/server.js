38f0cd3c1dbf583da3a87ce052c75d145072cb68
Long Dinh <longzdinh@gmail.com>
2018-01-19 21:35:19 +0700

    add heroku support

--- server/server.js
+++ server/server.js
@@ -2,11 +2,12 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.listen(port, () => {
+    console.log('Server started on port ', port);
 })
 console.log(publicPath);
